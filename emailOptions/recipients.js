0ee3fd7e46b9ff152f9c5be4998a0ef84765a5b6
Francis Ge <francisge527@gmail.com>
2026-03-01 22:34:13 +0800

    delete attachments folder and emailOptions folder

--- emailOptions/recipients.js
+++
@@ -1,7 +0,0 @@
-export const recipients = [
-  { name: "Arcedillo Babao", email: "ollidecrababaoaborde@gmail.com" },
-  { name: "Master Pogi Fajutnao", email: "andrewcitofajutnao@gmail.com" },
-  { name: "Nop Ily Cy", email: "xyrieltalledo0@gmail.com" },
-  { name: "Ernesto Argana", email: "ernestoargana911@gmail.com" },
-  { name: "Joseph Labuguen", email: "labuguenkarl35@gmail.com"},
-];
